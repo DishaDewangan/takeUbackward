f2f5834edd573c36e1bebacfa4526c3213609eac
Disha Dewangan <dishadewangan0109@gmail.com>
2025-01-11 15:09:21 +0530

    Update instructionsData.jsx

--- src/utils/instructionsData.jsx
+++ src/utils/instructionsData.jsx
@@ -3,7 +3,7 @@ export const instructionsData={
   data: [
     "This project name 'TakeUBackward' got its name ironically from the educational platform 'TakeUForward' by the OG Striver. While TakeUForward focuses on modern technologies, TakeUBackward gives quiz questions on old programming languages.",
     "There are 10 questions covering both old and modern programming languages.",
-    "You will have four options to choose from for each question.",
+    "You will have 4 options to choose from for each question.",
     "You can select only one option per question.",
     "Click 'Next' after selecting an answer to proceed.",
     "Your score will be based on correct answers.",
